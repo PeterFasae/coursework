4a7eaae72d549dd8c56804d4ee7cc93703b0aa0a
Peter Fasae <pfasae@gmail.com>
2024-12-04 11:03:00 +0000

    switched to online

--- src/App.vue
+++ src/App.vue
@@ -76,7 +76,7 @@ const submitForm = async () => {
   };
 
   try {
-    const response = await fetch('http://localhost:3000/api/orders', {
+    const response = await fetch('https://coursework-backend.onrender.com/api/orders', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(order),
